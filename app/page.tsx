71716c60855a0262ca713cff7939a0e8b019b310
Hanna Bodnarchuk <hannabodnarchuk.1@gmail.com>
2025-09-27 13:20:35 +0300

    section 5 added

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Section_1 from "../components/Section_1";
 import Section_2 from "../components/Section_2";
 import Section_3 from "../components/Section_3";
 import Section_4 from "../components/Section_4";
+import Section_5 from "../components/Section_5";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Section_2 />
       <Section_3 />
       <Section_4 />
+      <Section_5 />
     </main>
   );
 }
---
+++ components/Section_5.tsx
@@ -0,0 +1,120 @@
+"use client";
+import React from "react";
+import type { RootState, AppDispatch } from "../app/store/store";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "../app/store/slices/mainSlice";
+
+/*
+  section_5: {
+    hearing_req: HearingRequest | null;
+  };
+
+  type HearingRequest = "Without" | "In-person" | "Telephone" | "Video";
+*/
+export default function Section_5() {
+  const dispatch: AppDispatch = useDispatch();
+  const hearing_req = useSelector(
+    (state: RootState) => state.main.section_5.hearing_req
+  );
+
+  function handleHearingReq(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.currentTarget.id;
+    switch (value) {
+      case "section_5.request.without":
+        dispatch(
+          actions.setTextField({
+            path: "section_5.hearing_req",
+            value: "Without",
+          })
+        );
+        break;
+      case "section_5.request.inperson":
+        dispatch(
+          actions.setTextField({
+            path: "section_5.hearing_req",
+            value: "In-person",
+          })
+        );
+        break;
+      case "section_5.request.telephone":
+        dispatch(
+          actions.setTextField({
+            path: "section_5.hearing_req",
+            value: "Telephone",
+          })
+        );
+        break;
+      case "section_5.request.video":
+        dispatch(
+          actions.setTextField({
+            path: "section_5.hearing_req",
+            value: "Video",
+          })
+        );
+        break;
+    }
+  }
+
+  return (
+    <div className="border-l border-r border-b flex flex-wrap text-base">
+      <div className="sectionHeader">
+        <div>
+          5. HEARING REQUEST - Check <u>one</u> box only.
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="px-3 text-xl">
+          <input
+            type="checkbox"
+            id="section_5.request.without"
+            name="section_5.request.without"
+            className="boldCheckbox"
+            checked={hearing_req === "Without"}
+            onChange={(e) => handleHearingReq(e)}
+          />
+          <label htmlFor="section_5.request.without">
+            Review on papers submitted WITHOUT an in-person hearing
+          </label>
+        </div>
+        <div className="ml-40 font-bold arial text-xl">OR</div>
+        <div className="px-3 text-xl">
+          <input
+            type="checkbox"
+            id="section_5.request.inperson"
+            name="section_5.request.inperson"
+            className="boldCheckbox"
+            checked={hearing_req === "In-person"}
+            onChange={(e) => handleHearingReq(e)}
+          />
+          <label htmlFor="section_5.request.inperson">
+            In-person hearing in Manhattan
+          </label>
+        </div>
+        <div className="px-3 text-xl">
+          <input
+            type="checkbox"
+            id="section_5.request.telephone"
+            name="section_5.request.telephone"
+            className="boldCheckbox"
+            checked={hearing_req === "Telephone"}
+            onChange={(e) => handleHearingReq(e)}
+          />
+          <label htmlFor="section_5.request.telephone">Telephone hearing</label>
+        </div>
+        <div className="px-3 text-xl">
+          <input
+            type="checkbox"
+            id="section_5.request.video"
+            name="section_5.request.video"
+            className="boldCheckbox"
+            checked={hearing_req === "Video"}
+            onChange={(e) => handleHearingReq(e)}
+          />
+          <label htmlFor="section_5.request.video">
+            Video conference using Microsoft Teams
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
