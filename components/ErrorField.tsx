f3f4bad0d70ed27d79ca2c49874ae51296048532
Hanna Bodnarchuk <hannabodnarchuk.1@gmail.com>
2025-09-30 18:33:51 +0300

    hints for mobile changed

--- components/ErrorField.tsx
+++ components/ErrorField.tsx
@@ -5,20 +5,15 @@ export default function ErrorField({ title }: { title: string }) {
 
   return (
     <div
-      className="relative inline-block"
+      className="relative"
       onClick={() => setShow(!show)}
       onBlur={() => setShow(false)}
-      tabIndex={0}
     >
       <div className="errorField" title={title}>
         !
       </div>
 
-      {show && (
-        <div className="absolute bottom-full mb-1 px-2 py-1 bg-black text-white text-sm rounded">
-          {title}
-        </div>
-      )}
+      {show && <div className="mobileHint">{title}</div>}
     </div>
   );
 }
