652c88dfb748246db8d29fe3de93f0fb4b42d905
Hanna Bodnarchuk <hannabodnarchuk.1@gmail.com>
2025-09-30 18:26:25 +0300

    hints for mobile added

--- components/ErrorField.tsx
+++ components/ErrorField.tsx
@@ -1,10 +1,24 @@
-"use client";
-import React from "react";
+import { useState } from "react";
 
 export default function ErrorField({ title }: { title: string }) {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="errorField" title={title}>
-      !
+    <div
+      className="relative inline-block"
+      onClick={() => setShow(!show)}
+      onBlur={() => setShow(false)}
+      tabIndex={0}
+    >
+      <div className="errorField" title={title}>
+        !
+      </div>
+
+      {show && (
+        <div className="absolute bottom-full mb-1 px-2 py-1 bg-black text-white text-sm rounded">
+          {title}
+        </div>
+      )}
     </div>
   );
 }
